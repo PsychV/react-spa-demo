06ddcc74b49de43fce6ca2f761a189fac2364ac0
PsychV <victorluquebuxade@gmail.com>
2019-11-07 13:09:28 +0100

    build script

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component{
   render(){
     return(
       <div className="App">
-        <h1> Hello, World! badddkjdjldjkjdlkj </h1>
+        <h1> Hello, World! dddd </h1>
       </div>
     );
   }
